b148618fafca55118fed4910a077607c9cec1e92
Vaibhav Sethia <vaibhavsethia1998@gmail.com>
2019-11-30 21:07:33 +0530

    Updates

---
+++ precache-manifest.ea4733fed6dc72449551c8445646a76a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43efcec00e481a87c5a9",
+    "url": "/Myportfolio/static/js/runtime~main.43efcec0.js"
+  },
+  {
+    "revision": "3b09006c32a157b947eb",
+    "url": "/Myportfolio/static/js/main.3b09006c.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Myportfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3b09006c32a157b947eb",
+    "url": "/Myportfolio/static/css/main.796d749c.chunk.css"
+  },
+  {
+    "revision": "33644914aec19952e2a1a9d61696cd7e",
+    "url": "/Myportfolio/index.html"
+  }
+];
